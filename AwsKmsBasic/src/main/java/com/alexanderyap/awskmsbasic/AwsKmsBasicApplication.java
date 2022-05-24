1daa76453d2f80707b17385bc67f9a489eef82d6
Alex Yap <alexyap1205@live.com>
2022-05-25 08:48:10 +1000

    Base project

---
+++ AwsKmsBasic/src/main/java/com/alexanderyap/awskmsbasic/AwsKmsBasicApplication.java
@@ -0,0 +1,13 @@
+package com.alexanderyap.awskmsbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsKmsBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsKmsBasicApplication.class, args);
+    }
+
+}
---
+++ AwsKmsBasic/src/test/java/com/alexanderyap/awskmsbasic/AwsKmsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexanderyap.awskmsbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsKmsBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
